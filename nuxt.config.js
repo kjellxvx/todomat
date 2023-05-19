6f0192bdff598c57edddd72628193a95faac017d
kjellxvx <kjell.xvx@t-online.de>
2023-05-19 12:12:43 +0200

    changed title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     mode: 'spa',
     head: {
-      title: 'My Nuxt App',
+      title: 'Todomat',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
