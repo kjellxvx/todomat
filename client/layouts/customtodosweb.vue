ea8a56a2d281767e1a86ef9e4d67b8bfcfb51a1c
kjellxvx <kjell.xvx@t-online.de>
2023-07-16 19:20:56 +0200

    overflow media mobile

--- client/layouts/customtodosweb.vue
+++ client/layouts/customtodosweb.vue
@@ -7,14 +7,19 @@
 <style>
 html {
   height: 100vh;
-  width: 100%
+  width: 100%;
 }
 
 .page-container {
   height: 100vh;
   max-height: 100%;
-  width: 100%
+  width: 100%;
+  overflow: scroll;
 }
 
-
+@media only screen and (max-width: 1024px) {
+  .page-container {
+    overflow: normal;
+  }
+}
 </style>
