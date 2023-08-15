a29f1061627456f7adc4d7f263cf78394c2fdc24
kjellxvx <kjell.xvx@t-online.de>
2023-08-15 10:21:20 +0200

    changed print pdf

--- client/layouts/custom.vue
+++ client/layouts/custom.vue
@@ -9,14 +9,16 @@ html {
   /* width: 283px;
   height: 425px; */
   width: 566px;
-  height: 850px;
+  height: 864px;
 }
 
 .page-container {
   /* width: 283px;
   height: 425px; */
+  /* width: 566px;
+  height: 850px; */
   width: 566px;
-  height: 850px;
+  height: 867px;
   overflow: hidden;
 }
 
