000f887bd732b4fd4ec98195676434d0de2b148d
kjellxvx <kjell.xvx@t-online.de>
2023-09-07 18:13:59 +0200

    pb ip

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const pb = new PocketBase("http://127.0.0.1:8090");
+// const pb = new PocketBase("http://127.0.0.1:8090");
+const pb = new PocketBase("http://185.162.250.233:10017");
 
 let user_data = {};
 
@@ -30,22 +31,22 @@ app.post("/save", async (req, res) => {
 
     // if user has no token create new record, save data and return the record.id to the client
     if (token == false) {
-
-
       // generate token
       function makeid(length) {
-        let result = '';
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        let result = "";
+        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         const charactersLength = characters.length;
         let counter = 0;
         while (counter < length) {
-          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+          result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+          );
           counter += 1;
         }
         return result;
       }
 
-      const new_token = 'TODOMAT' + makeid(6);
+      const new_token = "TODOMAT" + makeid(6);
 
       user_data[new_token] = {
         token: new_token,
@@ -53,8 +54,9 @@ app.post("/save", async (req, res) => {
         todos: { ...todos },
       };
 
-
-      const record = await pb.collection("user_data").create(user_data[new_token]);
+      const record = await pb
+        .collection("user_data")
+        .create(user_data[new_token]);
       console.log("Saved data using new record: " + new_token);
       console.log(user_data[new_token]);
       res.status(200).json({
@@ -65,8 +67,6 @@ app.post("/save", async (req, res) => {
 
       // if user has a token update the data using the token
     } else {
-      
-
       user_data[token] = {
         token: token,
         data: { ...data },
@@ -74,10 +74,11 @@ app.post("/save", async (req, res) => {
       };
 
       // get id
-      const entry = await pb.collection('user_data').getFirstListItem('token="' + token + '"', {
-        expand: 'relField1,relField2.subRelField',
-      });
-
+      const entry = await pb
+        .collection("user_data")
+        .getFirstListItem('token="' + token + '"', {
+          expand: "relField1,relField2.subRelField",
+        });
 
       const record = await pb
         .collection("user_data")
@@ -116,9 +117,11 @@ app.get("/retrieve", async (req, res) => {
       expand: "relField1,relField2.subRelField",
     });*/
 
-    const userData = await pb.collection('user_data').getFirstListItem('token="' + token + '"', {
-      expand: 'relField1,relField2.subRelField',
-    });
+    const userData = await pb
+      .collection("user_data")
+      .getFirstListItem('token="' + token + '"', {
+        expand: "relField1,relField2.subRelField",
+      });
 
     // console.log("userData requested by user: " + token);
     return res.status(200).json({ userData });
