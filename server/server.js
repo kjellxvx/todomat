5d081ce86114c7462052f2bfc9b4298817af2ecb
kjellxvx <kjell.xvx@t-online.de>
2023-09-07 18:19:37 +0200

    require dotenv

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-//require("dotenv").config();
+require("dotenv").config();
 const PocketBase = require("pocketbase/cjs");
 const express = require("express");
 const bodyParser = require("body-parser");
