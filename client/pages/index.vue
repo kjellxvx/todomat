c82385fba500f01f1e70167b786f7a7c8789e26c
kjellxvx <kjell.xvx@t-online.de>
2023-11-15 00:08:48 +0100

    revert

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -180,8 +180,8 @@ onMounted(() => {
   position: fixed;
   top: 0;
   left: 0;
-  width: 100lvw;
-  height: 100lvh;
+  width: 100%;
+  height: 100%;
   /* width: 100vw;
   height: 100vh; */
   background-color: #131126;
